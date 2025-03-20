976df1f97e1a6f017eff522e1c7ce60dd12b5d09
rene1129 <123540167+rene1129@users.noreply.github.com>
2025-03-19 22:24:12 -0500

    second commit on new branch? we'll see!

--- firstGithubproject.py
+++ firstGithubproject.py
@@ -6,3 +6,5 @@ for calculation in range (0, 100):
     if user    
     ## idkkkk what just happened but im here i showed up maybe not for leetcode, but im here for git and im trying i will commit or repo idk but i will fix it tmr 
     #again idk what i did
+
+    #what does this do idk but its a surprise for tmr :)
