060d5110195fe37ea7df28ab380ee5e7f7a59894
rene1129 <123540167+rene1129@users.noreply.github.com>
2025-03-19 22:23:14 -0500

    idk what this does but im moving forward

--- firstGithubproject.py
+++ firstGithubproject.py
@@ -4,3 +4,5 @@ calculation = float(input(": "))
 
 for calculation in range (0, 100):
     if user    
+    ## idkkkk what just happened but im here i showed up maybe not for leetcode, but im here for git and im trying i will commit or repo idk but i will fix it tmr 
+    #again idk what i did
